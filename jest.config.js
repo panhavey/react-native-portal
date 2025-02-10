86bd5d82fd5a28b86c0fd0deb64520cdb68c9cb4
panhavey <panhavey@gmail.com>
2025-02-10 15:35:28 +0700

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
-module.exports = () => ({
+module.exports = {
+  rootDir: '.',
   preset: 'react-native',
   modulePathIgnorePatterns: [
     '<rootDir>/example/node_modules',
     '<rootDir>/lib/',
   ],
-});
+  moduleNameMapper: {
+    '@gorhom/portal': '<rootDir>/src',
+  },
+};
